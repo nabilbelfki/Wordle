7c650e064da834e83c96581c0f5f4d59308021c7
Nabil Belfki <nabilbelfki@gmail.com>
2026-04-25 23:09:19 -0400

    Added Google Analytics Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Settings from "./components/Settings/Settings";
 import { ThemeProvider } from "./context/ThemeContext";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -70,6 +71,9 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
         <ThemeProvider>
           <Settings />
           {children}
